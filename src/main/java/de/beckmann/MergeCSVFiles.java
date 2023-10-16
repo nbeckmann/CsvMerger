95ff64625947b963d32ad760ffc761ceb5fc4b3d
Nico Beckmann <nico.beckmann>
2023-10-16 22:18:02 +0200

    changed filename lookup

--- src/main/java/de/beckmann/MergeCSVFiles.java
+++ src/main/java/de/beckmann/MergeCSVFiles.java
@@ -5,13 +5,19 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MergeCSVFiles {
+    private static final String SEPARATOR = ";";
+    private static final String LINEBREAK = "\n";
+
     public static void main(String[] args) {
-        String[] fileNames = {"file1.csv", "file2.csv", "file3.csv"}; // Add your CSV file names here
-        String outputFileName = "merged.csv"; // Output file name
+
+        List<String> fileNames = fileNames(args);
+        String outputFileName = outputFile(args);
 
         Map<String, String> mergedData = new HashMap<>();
 
@@ -19,11 +25,11 @@ public class MergeCSVFiles {
             try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    String[] parts = line.split(",");
+                    String[] parts = line.split(SEPARATOR);
                     if (parts.length >= 2) {
                         String id = parts[0];
                         String data = parts[1];
-                        mergedData.merge(id, data, (existingData, newData) -> existingData + "," + newData);
+                        mergedData.merge(id, data, (existingData, newData) -> existingData + SEPARATOR + newData);
                     }
                 }
             } catch (IOException e) {
@@ -33,12 +39,23 @@ public class MergeCSVFiles {
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
             for (Map.Entry<String, String> entry : mergedData.entrySet()) {
-                writer.write(entry.getKey() + "," + entry.getValue() + "\n");
+                writer.write(entry.getKey() + SEPARATOR + entry.getValue() + LINEBREAK);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         System.out.println("Merged data saved to " + outputFileName);
+    }
+
+    private static List<String> fileNames(String[] args) {
+        List<String> fileNames = Arrays.asList("C:\\Users\\nicob\\Documents\\result1.csv",
+                "C:\\Users\\nicob\\Documents\\result2.csv", "C:\\Users\\nicob\\Documents\\result3.csv");
+
+        return fileNames;
+    }
+
+    private static String outputFile(String[] args) {
+        return "C:\\Users\\nicob\\Documents\\merged.csv";
     }
 }
